d77d0ea5ce9cfb08da00bd6fd6ff5ae86087bbb9
Marcin Klusek <s13859@pjwstk.edu.pl>
2020-01-05 20:24:39 +0100

    Revert "Client: wysyłanie wiadomości"
    This reverts commit f5c4001d0411bb474c02f46b412c9519206535c2.

--- ClientServer/src/Server.java
+++ ClientServer/src/Server.java
@@ -1,115 +1,14 @@
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+public class Server {
 
-public class Server extends JFrame{
-	
-	private static final int PORT = 4999;
-	private static ServerSocket ss;
-	private static int amountOfClients = 0;
-	private static ArrayList<Message> wiadomosci = new ArrayList<Message>();
-	private static double listVer = 1.0;
-	private JScrollPane jcpChat;
-	private JTextArea chat = new JTextArea(15,80);
-	
-	public static void main(String[] args) {
-		new Server();
-	}
-	
-	public Server() {
-		setSize(new Dimension(500, 400));
-		setLocation(100, 100);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setTitle("Serwer czatu grupowego");
-		setResizable(false);
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(4999);
+		Socket s = ss.accept();
 		
-		setGui();
-		
-		startServer();
-//		newMessage(new Message("17:00", "Klusek", "Cześć!"));
-//		newMessage(new Message("17:01", "Ewa", "Czego"));
-//		newMessage(new Message("17:02", "Klusek", "a nic specjlanego"));
-//		newMessage(new Message("17:02", "Klusek", "moze sie poznamy?"));
-//		newMessage(new Message("17:03", "Ewa", "po co?"));
-//		newMessage(new Message("17:04", "Klusek", "zeby sie poznac"));
-		
+		System.out.println(s.getPort() + " client is connected.");
+
 	}
 
-	private void startServer() {
-		try {
-			ss = new ServerSocket(PORT);
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "Podczas startu serwera program napotkał problem.\nServer error: 1");
-			e.printStackTrace();
-		}
-		
-		JOptionPane.showMessageDialog(null, "Serwer wystartował bez problemu. Trwa nasłuchiwanie...");
-		while (true) {
-			Socket socket = new Socket();
-            try {
-            	socket = ss.accept();
-            } catch (IOException e) {
-                System.out.println("I/O error: " + e);
-            }
-            //gdy klient polaczy sie z serwerem.
-
-    		System.out.println("\n\nServer: client nr. " + ++amountOfClients + " is connected on port ");
-            new EchoThread(socket, this).start();
-        }
-	}
-
-	private void setGui() {
-		Container pane = getContentPane();
-		
-		jcpChat = new JScrollPane(chat);
-        chat.setLineWrap(true);
-        chat.setWrapStyleWord(true);
-        
-		chat.setEditable(false);
-		
-		pane.add(jcpChat);
-		
-		chat.append("\tSerwer wystartował:\t" + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()));
-		pack();
-		repaint();
-	}
-
-	protected void newMessage(Message m) {
-		Message lastMsg = new Message("", "", "");
-		if(wiadomosci.size()>0)
-			lastMsg = wiadomosci.get(wiadomosci.size()-1);
-		wiadomosci.add(m); listVer += 0.01;
-		if(lastMsg.getAutor().equals(m.getAutor())) {
-			chat.append("\n" + m.getTresc());
-		}else {
-			chat.append("\n\n" + m.getCzas() + "    " + m.getAutor() + "\n" + m.getTresc());
-		}
-		System.out.println("Wersja listy: " + listVer);
-		
-		JScrollBar vertical = jcpChat.getVerticalScrollBar();
-		vertical.setValue( vertical.getMaximum() );
-	}
 }
-
-
-/*TODO
- * lista aktywnych uzytkowniku(user nickname) jlist.repaint() jako refresh
- */
